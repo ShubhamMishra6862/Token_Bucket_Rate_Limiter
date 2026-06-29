ac98694d71d5b25487827e48968722c57b93ff6b
ShubhamMishra6862 <shubham.mishra6862@gmail.com>
2026-06-30 03:18:16 +0530

    Did render change

--- src/main/java/com/sm/ratelimiter/config/RedisProperties.java
+++ src/main/java/com/sm/ratelimiter/config/RedisProperties.java
@@ -7,42 +7,35 @@ import org.springframework.stereotype.Component;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-/**
- * Wrapper class to prevent JedisPool from being exposed as an MBean.
- * This avoids JMX registration conflicts.
- */
-class JedisPoolWrapper extends JedisPool {
-    public JedisPoolWrapper(JedisPoolConfig poolConfig, String host, int port, int timeout) {
-        super(poolConfig, host, port, timeout);
-    }
-}
 
 @Component
 @Data
 @ConfigurationProperties(prefix = "spring.data.redis")
 public class RedisProperties {
-    
-    private String host = "localhost";
 
-    private int port = 6379;
+    private String host;
+    private int port;
+    private int timeout;
+    private String password;
+    private Ssl ssl = new Ssl();
 
-    private int timeout = 2000;
-
-    //Java client library for Redis. 
-    //Lets Java application communicate with Redis server
+    @Data
+    public static class Ssl {
+        private boolean enabled;
+    }
 
     @Bean(name = "jedisPool")
     public JedisPool getJedisPool() {
-        //Jedispool keeps multiple connections ready to reuse. 
+
         JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(50);
-        poolConfig.setMaxIdle(10);
-        poolConfig.setMinIdle(5);
-        poolConfig.setTestOnBorrow(true);
-        poolConfig.setTestOnReturn(true);
-        poolConfig.setTestWhileIdle(true);
-        poolConfig.setNumTestsPerEvictionRun(-1);
-        poolConfig.setMinEvictableIdleTimeMillis(60000);
-        return new JedisPoolWrapper(poolConfig, host, port, timeout);
+
+        return new JedisPool(
+                poolConfig,
+                host,
+                port,
+                timeout,
+                password,
+                ssl.isEnabled()
+        );
     }
 }
